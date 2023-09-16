c7eca853b73a9b2b757f106ca92c08b7673dd136
Lucas Alves <lucasalvesdk2@gmail.com>
2023-09-16 16:13:22 -0300

    Revert "Revert "Atualizações No Código.""
    This reverts commit f9a43e355a4f5d04d25c3076b5b372f8f9968068.

--- src/autenticacao.java
+++ src/autenticacao.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class autenticacao {
 
+    static Scanner scanner = new Scanner(System.in);
     public static boolean loginUsuario() {
+        System.out.println("Usuário: ");
+        String loginUser = scanner.next();
+        System.out.println("Senha: ");
+        String passUser = scanner.next();
+        if (loginUser.equals("user") && passUser.equals("user")) {
+            return true;
+        } else {
+            System.out.println("Usuario ou Senha Errado.");
+            menu.menuUsuarioLogin();
+        } return false;
+    }
+
+    public static boolean loginAdmin() {
 
         return true;
     }
--- src/clientes.java
+++ src/clientes.java
@@ -1,2 +1,10 @@
 public class clientes {
+    public clientes() {
+    }
+
+    public static void getInfos (String cadUser, String cadPass, String cadIdade){
+
+    }
+
+
 }
